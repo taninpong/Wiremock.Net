4d1232b5f8a5fd3af58c40463b5d92680e79cf5d
taninpong <thaninpong@perfenterprise.com>
2021-11-23 16:44:33 +0700

    Delete port 9092 and Use SSl

--- wmConsole/Program.cs
+++ wmConsole/Program.cs
@@ -80,7 +80,8 @@ namespace wmConsole
             var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
             _server = WireMockServer.Start(new WireMockServerSettings
             {
-                Urls = new[] { "http://*:9091/", "http://*:9092/" },
+                Urls = new[] { "https://*:9091/" },
+                UseSSL = true,
                 StartAdminInterface = true,
                 //"Authorization: Basic YWRtaW46cGFzcw==" (base64 for admin:pass)
                 AdminUsername = "admin",
@@ -132,17 +133,20 @@ namespace wmConsole
 
             _server.Given(Request.Create().WithPath("/CreateEmployeeByName")
                 .UsingPost()
-                .WithBody(new JsonPartialMatcher(
-                    JsonSerializer.Serialize(new { Name = "Joe" })
-                    , true)))
+                //.WithBody(new JsonPartialMatcher(
+                //    JsonSerializer.Serialize(new { Name = "Joe" })
+                //    , true))
+                )
                 .RespondWith(Response.Create().WithBody(
               "Create Success!").WithTransformer(true));
 
 
             _server.Given(Request.Create().WithPath("/CreateStudent")
-                .UsingPost())
+                .UsingPost()
+                .UsingAnyMethod()
+                )
+
                 .RespondWith(Response.Create()
-                .WithHeader("Content-Type", "application/json")
                 .WithBody(("Create Success {{request.bodyAsJson.name }}"))
                 .WithTransformer(true));
 
